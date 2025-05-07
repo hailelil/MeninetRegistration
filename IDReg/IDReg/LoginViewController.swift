707d9a8fc7fec44969fcc10c2463214bedfd2a64
Haileslassie Lilay <hailelil1999@gmail.com>
2025-05-07 13:09:33 +0300

    iOS based ID registration PoC

---
+++ IDReg/IDReg/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  IDReg
+//
+//  Created by HLD on 08/05/2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    //
+    
+}
---
+++ IDReg/IDReg/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  IDReg
+//
+//  Created by HLD on 07/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var SigninButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
